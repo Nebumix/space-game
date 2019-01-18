5efe04d7584110a09b99268733e2f7f18d487cc3
Mario <mario.alicicco@gmail.com>
2019-01-18 20:03:25 +0100

    sfondo fisso

--- game.js
+++ game.js
@@ -1,2 +1,34 @@
 "use strict";
 
+var canvas1 = document.getElementById("canvas"); 
+var contesto = canvas1.getContext("2d");
+
+// definisco il buffer 
+var buffer = document.createElement('canvas'); 
+buffer.width = canvas.width; 
+buffer.height = canvas.height;       
+var buffer_context = buffer.getContext('2d'); 
+
+
+var spazio = new Image();     
+spazio.src = "img/spazio1.jpg"; 
+
+// variabili globali 
+var posx = 350, posy = 300; 
+
+function draw(){
+
+    // scorrimento sfondo 
+    //2.2 e 3.6 sono il rapporto tra altezza e larghezza del canvas e dell'immagine
+    var shiftx = (posx - (canvas1.width/2)) / 2.2; 
+    var shifty = (posy - (canvas1.height/2)) / 3.6; 
+    buffer_context.drawImage(spazio, - 162 - shiftx, - 84 - shifty); 
+
+    contesto.drawImage(buffer, 0, 0); 
+
+    requestAnimationFrame(draw);
+}
+
+function init(){
+    draw();
+}
